e28f1d5921ca72821bfb001ffa260347add8b133
unknown <frc2815@gmail.com>
2021-03-28 17:35:35 -0400

    DriveTrain2 subsystem auto tragectory command

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -7,39 +7,56 @@
 
 package frc.robot;
 
+import edu.wpi.first.wpilibj.controller.RamseteController;
+import edu.wpi.first.wpilibj.kinematics.DifferentialDriveKinematics;
+
 /**
  * Robot constants
  */
 public class Constants {
     // the talons for driving and moving around and stuff
-    public static int[] driveTalonPorts = {0, 1, 2, 3};
-    public static int[] mainTalonPorts = {0, 3};  // talons which have the encoders
-    public static int[] bareTalonPorts = {1, 2};  // talons with no encoders
+    public static final int[] driveTalonPorts = {0, 1, 2, 3};
+    public static final int[] mainTalonPorts = {0, 3};  // talons which have the encoders
+    public static final int[] bareTalonPorts = {1, 2};  // talons with no encoders
 
     // weird thingy on robot that moves balls from collector to tower
-    public static int leftHopperPort = 0;
-    public static int rightHopperPort = 2;
+    public static final int leftHopperPort = 0;
+    public static final int rightHopperPort = 2;
 
     // ports for the spark maxes for powering the two neos which shoot balls
-    public static int rightShooterPort = 5;
-    public static int leftShooterPort = 4;
+    public static final int rightShooterPort = 5;
+    public static final int leftShooterPort = 4;
 
     // collector/intake thingy that picks up balls
-    public static int intakePort = 6;
+    public static final int intakePort = 6;
 
     // the spark for the redline which powers the tower
-    public static int towerPort = 1;
+    public static final int towerPort = 1;
     
-    public static double wheelCircumference = 18.5;
-    public static double gearboxRatio = 10.91;
+    public static final double wheelCircumference = 18.5;
+    public static final double gearboxRatio = 10.91;
 
-    public static double cameraHeight = -1;
-    public static double cameraFovX = 49.7;
-    public static double cameraFovY = 59.6;
+    public static final double cameraHeight = -1;
+    public static final double cameraFovX = 49.7;
+    public static final double cameraFovY = 59.6;
 
-	/**
-	 * Gains used in Positon Closed Loop, to be adjusted accordingly
-     * Gains(kp, ki, kd, kf, izone, peak output);
-     */
-    public static Gains kGains = new Gains(0.15, 0.0, 1.0, 0.0, 0, 1.0);
+    // PathWeaver stuff
+    public static final double ksVolts = 1.09;
+    public static final double kvVoltSecondsPerMeter = 6.22;
+    public static final double kaVoltSecondsSquaredPerMeter = 1.25;
+
+    public static final double kPDriveVel = 79.4;
+
+    public static final double kTrackwidthMeters = 0.2706473008527956;
+    public static final DifferentialDriveKinematics kDriveKinematics = new DifferentialDriveKinematics(kTrackwidthMeters);
+
+    public static final double kMaxSpeedMetersPerSecond = 3.0;
+    public static final double kMaxAccelerationMetersPerSecondSquared = 3.0;
+
+    public static final double kRamseteB = 2.0;
+    public static final double kRamseteZeta = 0.7;
+
+    public static final double kMagEncoderDistancePerPulse = 0.47877871986 / 4096;
+
+    
 }
--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -7,12 +7,26 @@
 
 package frc.robot;
 
+import java.util.List;
+
 import edu.wpi.first.wpilibj.ADXRS450_Gyro;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.command.CommandGroup;
+import edu.wpi.first.wpilibj.controller.PIDController;
+import edu.wpi.first.wpilibj.controller.RamseteController;
+import edu.wpi.first.wpilibj.controller.SimpleMotorFeedforward;
+import edu.wpi.first.wpilibj.geometry.Pose2d;
+import edu.wpi.first.wpilibj.geometry.Rotation2d;
+import edu.wpi.first.wpilibj.geometry.Translation2d;
+import edu.wpi.first.wpilibj.trajectory.Trajectory;
+import edu.wpi.first.wpilibj.trajectory.TrajectoryConfig;
+import edu.wpi.first.wpilibj.trajectory.TrajectoryGenerator;
+import edu.wpi.first.wpilibj.trajectory.constraint.DifferentialDriveVoltageConstraint;
+import edu.wpi.first.wpilibj2.command.RamseteCommand;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.commands.Drive;
 import frc.robot.commands.MoveHopper;
 import frc.robot.commands.MoveIntake;
@@ -20,6 +34,7 @@ import frc.robot.commands.MoveTower;
 import frc.robot.commands.ShootShooter;
 import frc.robot.commands.autoCommandGroups.TestAuto;
 import frc.robot.subsystems.DriveTrain;
+import frc.robot.subsystems.DriveTrain2;
 import frc.robot.subsystems.Hopper;
 import frc.robot.subsystems.Intake;
 import frc.robot.subsystems.Shooter;
@@ -32,14 +47,20 @@ public class RobotContainer {
   // Controllers
   private XboxController mano;
   private XboxController mano2;
-  // private final XboxController mano2 = DriverStation.getInstance().getJoystickName(1).isEmpty() ? mano : new XboxController(1);
+  // private final XboxController mano2 =
+  // DriverStation.getInstance().getJoystickName(1).isEmpty() ? mano : new
+  // XboxController(1);
 
   // Important stuff that isn't a controller, command, or subsytem
   private final String driveMode = "normal";
-  private final ADXRS450_Gyro gyro = new ADXRS450_Gyro();
+  public final static ADXRS450_Gyro gyro = new ADXRS450_Gyro();
+
+  RamseteController controller1 = new RamseteController();
+  RamseteController controller2 = new RamseteController(2.1, 0.8);
 
   // Subsystems
-  private final DriveTrain driveTrain = new DriveTrain();
+  // private final DriveTrain driveTrain = new DriveTrain();
+  private final DriveTrain2 driveTrain2 = new DriveTrain2();
   private final Tower tower = new Tower();
   private final Hopper hopper = new Hopper();
   private final Shooter shooter = new Shooter();
@@ -57,54 +78,105 @@ public class RobotContainer {
   }
 
   public void setup() {
-    mano = new XboxController(0);
+    // mano = new XboxController(0);
 
-    if (DriverStation.getInstance().getJoystickName(1).isEmpty()) {
-      mano2 = mano;
-    } else {
-      mano2 = new XboxController(1);
-    }
+    // if (DriverStation.getInstance().getJoystickName(1).isEmpty()) {
+    //   mano2 = mano;
+    // } else {
+    //   mano2 = new XboxController(1);
+    // }
 
-    if (driveMode.equals("trigger")) {
-      drive = new Drive(
-        driveTrain,
-        () -> (mano.getTriggerAxis(GenericHID.Hand.kRight) - mano.getTriggerAxis(GenericHID.Hand.kLeft)),
-        () -> mano.getX(GenericHID.Hand.kRight)
-      );
-    } else {
-      drive = new Drive(
-        driveTrain,
-        () -> mano.getY(GenericHID.Hand.kLeft),
-        () -> mano.getX(GenericHID.Hand.kRight)
-      );
-    }
+    // if (driveMode.equals("trigger")) {
+    //   drive = new Drive(driveTrain,
+    //       () -> (mano.getTriggerAxis(GenericHID.Hand.kRight) - mano.getTriggerAxis(GenericHID.Hand.kLeft)),
+    //       () -> mano.getX(GenericHID.Hand.kRight));
+    // } else {
+    //   drive = new Drive(driveTrain, () -> mano.getY(GenericHID.Hand.kLeft), () -> mano.getX(GenericHID.Hand.kRight));
+    // }
 
-    moveTower = new MoveTower(
-      tower,
-      () -> mano2.getBumper(GenericHID.Hand.kRight),
-      () -> mano2.getXButton(),
-      () -> mano2.getBumper(GenericHID.Hand.kLeft)
-    );
+    // moveTower = new MoveTower(tower, () -> mano2.getBumper(GenericHID.Hand.kRight), () -> mano2.getXButton(),
+    //     () -> mano2.getBumper(GenericHID.Hand.kLeft));
 
-    moveHopper = new MoveHopper(hopper, () -> mano2.getXButton());
-    shootShooter = new ShootShooter(shooter, () -> mano2.getYButtonPressed(), () -> mano2.getPOV());
+    // moveHopper = new MoveHopper(hopper, () -> mano2.getXButton());
+    // shootShooter = new ShootShooter(shooter, () -> mano2.getYButtonPressed(), () -> mano2.getPOV());
 
-    if (mano2.equals(mano)) {
-      moveIntake = new MoveIntake(intake, () -> (
-        mano.getTriggerAxis(GenericHID.Hand.kRight) - mano.getTriggerAxis(GenericHID.Hand.kLeft)
-      ));
-    } else {
-      moveIntake = new MoveIntake(intake, () -> mano2.getY(GenericHID.Hand.kLeft));
-    }
+    // if (mano2.equals(mano)) {
+    //   moveIntake = new MoveIntake(intake,
+    //       () -> (mano.getTriggerAxis(GenericHID.Hand.kRight) - mano.getTriggerAxis(GenericHID.Hand.kLeft)));
+    // } else {
+    //   moveIntake = new MoveIntake(intake, () -> mano2.getY(GenericHID.Hand.kLeft));
+    // }
 
-    driveTrain.setDefaultCommand(drive);
-    tower.setDefaultCommand(moveTower);
-    hopper.setDefaultCommand(moveHopper);
-    shooter.setDefaultCommand(shootShooter);
-    intake.setDefaultCommand(moveIntake);
+    // driveTrain.setDefaultCommand(drive);
+    // tower.setDefaultCommand(moveTower);
+    // hopper.setDefaultCommand(moveHopper);
+    // shooter.setDefaultCommand(shootShooter);
+    // intake.setDefaultCommand(moveIntake);
   }
 
-  public TestAuto getAutonomousCommand() {
-    return new TestAuto(driveTrain);
+  // public TestAuto getAutonomousCommand() {
+  //   return new TestAuto(driveTrain);
+  // }
+  /**
+   * Use this to pass the autonomous command to the main {@link Robot} class.
+   *
+   * @return the command to run in autonomous
+   */
+  public SequentialCommandGroup getAutonomousCommand() {
+
+    // Create a voltage constraint to ensure we don't accelerate too fast
+    var autoVoltageConstraint =
+        new DifferentialDriveVoltageConstraint(
+            new SimpleMotorFeedforward(Constants.ksVolts,
+                                       Constants.kvVoltSecondsPerMeter,
+                                       Constants.kaVoltSecondsSquaredPerMeter),
+            Constants.kDriveKinematics,
+            10);
+
+    // Create config for trajectory
+    TrajectoryConfig config =
+        new TrajectoryConfig(Constants.kMaxSpeedMetersPerSecond,
+                             Constants.kMaxAccelerationMetersPerSecondSquared)
+            // Add kinematics to ensure max speed is actually obeyed
+            .setKinematics(Constants.kDriveKinematics)
+            // Apply the voltage constraint
+            .addConstraint(autoVoltageConstraint);
+
+    // An example trajectory to follow.  All units in meters.
+    Trajectory exampleTrajectory = TrajectoryGenerator.generateTrajectory(
+        // Start at the origin facing the +X direction
+        new Pose2d(0, 0, new Rotation2d(0)),
+        // Pass through these two interior waypoints, making an 's' curve path
+        List.of(
+            new Translation2d(1, 1),
+            new Translation2d(2, -1)
+        ),
+        // End 3 meters straight ahead of where we started, facing forward
+        new Pose2d(3, 0, new Rotation2d(0)),
+        // Pass config
+        config
+    );
+
+    RamseteCommand ramseteCommand = new RamseteCommand(
+        exampleTrajectory,
+        driveTrain2::getPose,
+        new RamseteController(Constants.kRamseteB, Constants.kRamseteZeta),
+        new SimpleMotorFeedforward(Constants.ksVolts,
+                                   Constants.kvVoltSecondsPerMeter,
+                                   Constants.kaVoltSecondsSquaredPerMeter),
+        Constants.kDriveKinematics,
+        driveTrain2::getWheelSpeeds,
+        new PIDController(Constants.kPDriveVel, 0, 0),
+        new PIDController(Constants.kPDriveVel, 0, 0),
+        // RamseteCommand passes volts to the callback
+        driveTrain2::tankDriveVolts,
+        driveTrain2
+    );
+
+    // Reset odometry to the starting pose of the trajectory.
+    driveTrain2.resetOdometry(exampleTrajectory.getInitialPose());
+
+    // Run path following command, then stop at the end.
+    return ramseteCommand.andThen(() -> driveTrain2.tankDriveVolts(0, 0));
   }
 }
