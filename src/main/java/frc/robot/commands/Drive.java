ed739605c125c694866a1124ceaa44d2cbe68de3
unknown <frc2815@gmail.com>
2021-03-31 14:12:38 -0400

    remove unnecessary calculation to invert forwards/backwards axis

--- src/main/java/frc/robot/commands/Drive.java
+++ src/main/java/frc/robot/commands/Drive.java
@@ -39,7 +39,7 @@ public class Drive extends CommandBase {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    double forwardsValue = -forwardsAxis.getAsDouble();
+    double forwardsValue = forwardsAxis.getAsDouble();
     double turnValue = turnAxis.getAsDouble() * .8;
 
     if (Math.abs(forwardsValue) < .05) {
@@ -50,7 +50,7 @@ public class Drive extends CommandBase {
       turnValue = 0.0;
     }
 
-    driveTrain.curvatureDrive(-1 * forwardsValue, turnValue);
+    driveTrain.curvatureDrive(forwardsValue, turnValue);
   }
 
   // Called once the command ends or is interrupted.
